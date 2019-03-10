95a1d1ce33120eebe879e70be1aeee8c94cb489f
WojciechNiespielak <wojciech.niespielak@gmail.com>
2019-03-10 20:41:01 +0100

    initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] firstMatrix = new int[];
+int[][] secondMatrix = new int[][];
+
+for (int i=0; i <100; i++){
+
+    firstMatrix[i] = RandomIntNumber.getRandomNumber(0, 100);
+}
+
+for (int i =0; i <100; i++){
+
+    int loopMaxBreak = new int;
+    loopMaxBreak=i;
+
+    
+
+        }
+
+
+    }
+
+}
